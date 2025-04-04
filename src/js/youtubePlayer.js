2e8e153857b42413736b045057863c9626c97ca6
SaraMsoso <Sara.soder44@gmail.com>
2025-04-04 17:27:20 +0200

    Updating the explore the youtubePlayer JS codes

--- src/js/youtubePlayer.js
+++ src/js/youtubePlayer.js
@@ -39,11 +39,6 @@ window.onYouTubeIframeAPIReady = function () {
             onReady: () => {
                 console.log("YouTube Player is ready");
                 playerReadyResolve(); 
-
-                // Call the explore.js function
-                if (typeof initYouTubeSection === "function") {
-                    initYouTubeSection();
-                }
             }
         }
     });
